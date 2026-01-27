a127eaa2da83ca47f954737c5f820f55ec0d206b
sumeetkasar5-cyber <sumeetkasar5@gmail.com>
2026-01-27 20:56:56 +0530

    Add files via upload

---
+++ assignment task2.py
@@ -0,0 +1,7 @@
+first_name = "sumeet"
+last_name = "kasar"
+
+first_name + " " + last_name
+full_name = first_name + " " + last_name
+print("Hello", full_name, "welcome to the python!")
+
---
+++ assignment1.py
@@ -0,0 +1,15 @@
+num1=5
+num2=10
+add = num1 + num2
+sub = num1 - num2
+multiply = num1 * num2
+divide = num1 / num2 
+
+print(add)
+print(sub)
+print(multiply)
+print(divide)
+
+  
+
+
